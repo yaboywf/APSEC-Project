e25deca497a8f44919bab7569e952ae857d9d18a
Dylan <dylanyeowf@gmail.com>
2025-05-05 16:38:35 +0800

    Updated file formatting

--- server.js
+++ server.js
@@ -9,24 +9,47 @@ const { register, login, verify } = require("./controller/authFunctions");
 require("./controller/passport");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 require("dotenv").config();
 
 app.use(cors({
-	origin: ['http://localhost:3001', 'http://127.0.0.1:3001'],
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	credentials: true
+    origin: ['http://localhost:3001', 'http://127.0.0.1:3001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
 }));
 
 mongoose.set("strictQuery", true);
 mongoose.connect(process.env.DB_CONNECT)
-.then(() => console.log("Connected to DB!"))
-.catch((err) => console.log(err));
+    .then(() => console.log("Connected to DB!"))
+    .catch((err) => console.log(err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: process.env.APP_SECRET, resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
+
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "'unsafe-inline'"],
+            objectSrc: ["'none'"],
+            upgradeInsecureRequests: [],
+        },
+    },
+    referrerPolicy: { policy: 'no-referrer' },
+    frameguard: { action: 'deny' },
+    dnsPrefetchControl: { allow: false },
+    hidePoweredBy: true,
+    xssFilter: true,
+    noSniff: true,
+    StrictTransportSecurity: {
+        maxAge: 63072000,
+        includeSubDomains: true,
+        preload: true,
+    },
+}));
+
 
 app.post("/api/auth/register", (req, res) => {
     register(req.body, res);
@@ -36,19 +59,19 @@ app.post("/api/auth/login", passport.authenticate('local', { session: false }), 
     login(req, res);
 });
 
-app.get("/api/admin", verify(role=["admin"]), (req, res) => {
+app.get("/api/admin", verify(role = ["admin"]), (req, res) => {
     return res.json({ message: "You have access to an admin only page", user: req.user });
 });
 
-app.get("/api/teacher", verify(role=["teacher"]), (req, res) => {
+app.get("/api/teacher", verify(role = ["teacher"]), (req, res) => {
     return res.json({ message: "You have access to an teacher only page", user: req.user });
 });
 
-app.get("/api/ta", verify(role=["teacher-assistant"]), (req, res) => {
+app.get("/api/ta", verify(role = ["teacher-assistant"]), (req, res) => {
     return res.json({ message: "You have access to an teacher assistant only page", user: req.user });
 });
 
-app.get("/api/student", verify(role=["student"]), (req, res) => {
+app.get("/api/student", verify(role = ["student"]), (req, res) => {
     return res.json({ message: "You have access to an student only page", user: req.user });
 });
 
