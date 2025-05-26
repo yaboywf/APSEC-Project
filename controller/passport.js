bdff4edb94059f00e9dcfb3bad1d95fc9579c5cc
Dylan <dylanyeowf@gmail.com>
2025-05-26 22:46:12 +0800

    fixed bug in code not allowing users to login should their username have special characters

--- controller/passport.js
+++ controller/passport.js
@@ -13,7 +13,6 @@ const validator = require('validator');
 passport.use(new LocalStrategy(
     async (username, password, done) => {
         try {
-            if (!validator.isAlphanumeric(username)) return done(null, false, { message: 'Invalid username or password' });
             const sanitizedName = validator.escape(username);
             const user = await User.findOne({ name: sanitizedName });
             if (!user) return done(null, false, { message: 'Invalid username or password' });
